70ee42313ca4a3e87e2bbe955c6eb786653c250e
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-02-01 10:47:16 +0100

    Modificado prefab Player
    Modificado prefab de PlayerArmature para tener los scripts

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -9,18 +9,26 @@ using UnityEngine.InputSystem;
 public class Teleport : MonoBehaviour
 {
 
-    public GameObject Marca;
-    public bool crearMarca=false;
-    
+    public GameObject Marca=null;
+    public bool crearMarca = false;
+    public Vector3 posicionDeLaMarca;
+    public GameObject nuevaMarca;
 
-  
+
+
     void Update()
     {
         if (Keyboard.current.mKey.isPressed && !crearMarca)
         {
             CrearMarca();
             crearMarca = true;
+        }
 
+        if (Keyboard.current.tKey.isPressed && crearMarca)
+        {
+
+            Teleportarse();
+            
 
         }
     }
@@ -32,12 +40,34 @@ public class Teleport : MonoBehaviour
         Vector3 posicionJugador = transform.position;
 
         // Crea la "Marca" en la posición del jugador
-        GameObject nuevaMarca = Instantiate(Marca, posicionJugador, Quaternion.identity);
+        nuevaMarca = Instantiate(Marca, posicionJugador, Quaternion.identity);
 
-     
+        // Guarda la posición de la marca en la variable
+        posicionDeLaMarca = nuevaMarca.transform.position;
 
+    }
+
+    void Teleportarse()
+    {
+        // Teleporta al jugador a la posición almacenada en posicionDeLaMarca
+        transform.position = posicionDeLaMarca;
        
+        
+        // Destruye el objeto nuevaMarca
+        if (nuevaMarca != null)
+        {
+            Destroy(nuevaMarca);
+        }       
 
+
+        // Llama a CambiarMarca después de un pequeño retraso (0.1 segundos)
+        Invoke("CambiarMarca", 0.1f);
+
+
+    }
+
+    void CambiarMarca() {
+        crearMarca = false;
     }
 }
 
