600c662500244649960e30e9397be96a9d1bf862
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-02-05 10:22:10 +0100

    Creada conexion con ip cliente

--- Assets/Scripts/NetworkBotones.cs
+++ Assets/Scripts/NetworkBotones.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 
 public class NetworkButtons : MonoBehaviour
 {
+    private string IP = "Client IP";
+    private ushort serverPort = 7777;
+
     private void OnGUI()
     {
+
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
+            
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Client") && !string.IsNullOrEmpty(IP) &&!IP.Equals("Client IP"))
+            {
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+                    IP,  // IP que entra por el input
+                    serverPort // Puerto server
+                );
+                NetworkManager.Singleton.StartClient();
+            }
+            IP = GUILayout.TextField(IP, 25);
         }
 
         GUILayout.EndArea();
