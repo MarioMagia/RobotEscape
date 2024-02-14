d2d519babd9de045ec6afc5bb3005490d57d2a2d
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-02-14 10:57:11 +0100

    Pruebas sonido

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -6,14 +6,25 @@ public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject puertaInvisible;
+    AudioSource audioSource;
+
+    [SerializeField] private AudioClip sonido;
+
+
+    private void Awake()
+    {
+        audioSource.GetComponent<AudioSource> ();
+    }
     public void Abrir()
     {
         animator.SetBool("isOpen", true);
+        audioSource.PlayOneShot(sonido);
         puertaInvisible.SetActive(false);
     }
     public void Cerrar()
     {
         animator.SetBool("isOpen", false);
+        audioSource.PlayOneShot(sonido);
         puertaInvisible.SetActive(true);
     }
 }
