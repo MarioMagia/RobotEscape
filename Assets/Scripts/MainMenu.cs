32b0d6f32d2bae39f55c62a93954de47115b60e0
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-02-16 10:39:10 +0100

    Cambiado menu para entrar a leveltutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void Play()
     {
-        SceneManager.LoadScene("Playground");   
+        SceneManager.LoadScene("LevelTutorial");   
     }
 
     // Update is called once per frame
