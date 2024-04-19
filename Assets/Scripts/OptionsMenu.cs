303040f9328b93b6f20ed7108b3a42f08a5a6d70
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-04-19 10:01:39 +0200

    Creado script para mostrar el cambio de control de keyboard a gamepad en options

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -6,21 +6,25 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
-{ 
-   
+{
+
 
     public AudioMixer audioMixer;
 
-    public Dropdown resolutionDropdown;   
-    public Dropdown renderDropdown;   
-   
+    public Dropdown resolutionDropdown;
+    public Dropdown renderDropdown;
+    public TMP_Dropdown controlsDropdown;
+    public GameObject keyboardControlsPanel;
+    public GameObject gamepadControlsPanel;
+
+
 
     Resolution[] resolutions;
 
 
     private void Awake()
     {
-       
+
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
@@ -39,9 +43,9 @@ public class OptionsMenu : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-    }     
-   
-   
+    }
+
+
 
     public void SetQuality(int qualityIndex)
     {
@@ -73,6 +77,36 @@ public class OptionsMenu : MonoBehaviour
 
         //Guardamos el volumen de los SFX que acabamos de configurar en Playerprefs para poder coger ese valor en cualquier otra scene o al iniciar el juego
         PlayerPrefs.SetFloat("volumeSFX", volumeSFX);
+    }
+
+
+    public void Controls()
+    {
+        // Coge el valor que se ha escogido del dropdown de control(Keyboard = 0, Gamepad = 1)
+        controlsDropdown.onValueChanged.AddListener(delegate
+        {
+            DropdownValueChanged(controlsDropdown);
+        });
+        keyboardControlsPanel.SetActive(true); //Al iniciar muestra los controles de keyboard
+        gamepadControlsPanel.SetActive(false);
+        controlsDropdown.value = 0;
+        controlsDropdown.Select();//Muestra el tipo de control que se ha selecionado
+
+    }
+
+    // Coge el valor que se ha escogido del dropdown de control y muestra el selecionado
+    void DropdownValueChanged(TMP_Dropdown change)
+    {
+        if (change.value == 0) // Keyboard Controls selected
+        {
+            gamepadControlsPanel.SetActive(false);
+            keyboardControlsPanel.SetActive(true);
+        }
+        else if (change.value == 1) // Gamepad Controls selected
+        {
+            keyboardControlsPanel.SetActive(false);
+            gamepadControlsPanel.SetActive(true);
+        }
     }
 
 }
