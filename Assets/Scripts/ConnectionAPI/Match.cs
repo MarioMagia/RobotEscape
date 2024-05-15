0f71c36ab746b7595586c3d7b4bfac36c4fe7d91
a22marramgar <a22marramgar@inspedralbes.cat>
2024-05-15 10:51:18 +0200

    Update Match.cs

--- Assets/Scripts/ConnectionAPI/Match.cs
+++ Assets/Scripts/ConnectionAPI/Match.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,7 +8,79 @@ using UnityEngine.SceneManagement;
 
 public class Match : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public void EnterStage(string name)
+    {
+        StartCoroutine(GetStageAPI(Settings.URL + "/stages/getStageByName/"+name));
+    }
+
+    IEnumerator GetStageAPI(string url)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                string responseText = request.downloadHandler.text;
+                StageData stageData = JsonUtility.FromJson<StageData>(responseText);
+                if (request.responseCode == 200)
+                {
+                    PlayerPrefs.SetInt("StageId", stageData.id);
+                    MatchStageBody body = new MatchStageBody();
+                    body.id_stage = stageData.id;
+                    body.id_match = PlayerPrefs.GetInt("MatchId");
+                    StartCoroutine(PostAPI(Settings.URL + "/match_stage/enterStage", JsonUtility.ToJson(body)));
+                }
+            }
+            else
+            {
+                Debug.Log("Error: " + request.error);
+                FindFirstObjectByType<TMP_Text>().text = request.error;
+            }
+        }
+    }
+
+    IEnumerator PostAPI(string url, string body)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Post(url, body, "application/json"))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                if (request.responseCode == 200)
+                {
+                    Debug.Log(url+" success");
+                }
+                else
+                {
+                    Debug.Log(url+" failure");
+                }
+            }
+            else
+            {
+                Debug.Log("Error: " + request.error);
+                FindFirstObjectByType<TMP_Text>().text = request.error;
+            }
+        }
+    }
+
+    public void EndStage(ArrayList checkpointTimes)
+    {
+        MatchStageBody body = new MatchStageBody();
+        body.id_stage = PlayerPrefs.GetInt("StageId");
+        body.id_match = PlayerPrefs.GetInt("MatchId");
+        RoomTimeBody[] rooms = new RoomTimeBody[checkpointTimes.Count];
+        for (int i = 0; i < checkpointTimes.Count; i++)
+        {
+            KeyValuePair<string, string> room = (KeyValuePair<string, string>)checkpointTimes[i];
+            rooms[i] = new RoomTimeBody();
+            rooms[i].id = room.Key;
+            rooms[i].time = room.Value;
+            // Do something with each item
+        }
+        body.rooms = rooms;
+        StartCoroutine(PostAPI(Settings.URL + "/match_stage/endStage", JsonUtility.ToJson(body)));
+    }
+
     public bool StartMatch(int idHost,int idClient)
     {
         StartMatchBody body = new StartMatchBody();
@@ -25,10 +98,10 @@ public class Match : MonoBehaviour
             if (request.result == UnityWebRequest.Result.Success)
             {
                 string responseText = request.downloadHandler.text;
-                MessageData messageData = JsonUtility.FromJson<MessageData>(responseText);
+                StartMatchData matchData = JsonUtility.FromJson<StartMatchData>(responseText);
                 if (request.responseCode == 200)
                 {
-                    SceneManager.LoadScene("MainMenu");
+                    PlayerPrefs.SetInt("MatchId", matchData.insertId);
                 }
             }
             else
@@ -38,6 +111,34 @@ public class Match : MonoBehaviour
             }
         }
     }
+}
+[System.Serializable]
+public class StageData
+{
+    public int id;
+    public string name;
+    public int order;
+    public int times_played;
+    public int times_completed;
+    public int countdown_timer;
+}
+[System.Serializable]
+public class MatchStageBody
+{
+    public int id_match;
+    public int id_stage;
+    public RoomTimeBody[] rooms;
+}
+[System.Serializable]
+public class RoomTimeBody
+{
+    public string id;
+    public string time;
+}
+[System.Serializable]
+public class StartMatchData
+{
+    public int insertId;
 }
 [System.Serializable]
 public class StartMatchBody
